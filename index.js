4490f0bc5ed917b1d80829c9b14074ab9993c787
Jagrutee <jagrutee.banda@ril.com>
2017-12-22 00:37:17 +0530

    added getLanguage function and exported

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const unicodeMappings = require('./unicodeMappings.json');
 const async = require('async');
-let probability = 0;
-let language = '';
-const detectText = process.argv[2];
-const unicodeMappingsKeys = Object.keys(unicodeMappings);
+const unicodeMappings = require('./unicodeMappings.json');
 
-for (var i = 0; i < detectText.length; i++) {
-  const charCode = detectText.charCodeAt(i);
-  async.map(unicodeMappingsKeys, function (unicodeMappingsKey, unicodeMappingsKeysCallback) {
-    if (unicodeMappingsKey) {
-      if (charCode > parseInt(`0x${unicodeMappings[unicodeMappingsKey].minUnicode}`) && charCode < parseInt(`0x${unicodeMappings[unicodeMappingsKey].maxUnicode}`)) {
-        probability = probability + 1;
-        language = unicodeMappings[unicodeMappingsKey].language;
-      }
-    }
-  });
+if (process.argv[2].length > 0) {
+  console.log(getLanguage(process.argv[2]));;
 }
 
-console.log(language);
+function getLanguage(detectText) {
+  const unicodeMappingsKeys = Object.keys(unicodeMappings);
+  let probability = 0;
+  let detectedLanguage = '';
+  for (var i = 0; i < detectText.length; i++) {
+    const charCode = detectText.charCodeAt(i);
+    async.map(unicodeMappingsKeys, function (unicodeMappingsKey, unicodeMappingsKeysCallback) {
+      if (unicodeMappingsKey) {
+        if (charCode > parseInt(`0x${unicodeMappings[unicodeMappingsKey].minUnicode}`) && charCode < parseInt(`0x${unicodeMappings[unicodeMappingsKey].maxUnicode}`)) {
+          probability = probability + 1;
+          detectedLanguage = unicodeMappings[unicodeMappingsKey].language;
+        }
+      }
+    });
+  }
+  if (probability/detectText.length > 0.5) {
+    return detectedLanguage;
+  } else {
+    return 'Please check the text!';
+  }
+}
+
+module.exports = {
+  getLanguage
+};
